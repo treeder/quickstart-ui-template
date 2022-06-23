5880f771a41bb7e7ccd5c122f3784afbe775f15c
Travis Reeder <treeder@gmail.com>
2022-06-23 08:53:06 -0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -68,5 +68,11 @@ class ApiError extends Error {
     }
 }
 
+export function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 export { zapi };
 
